79a8ff409b46ff1dd66f8cf538c25ad007371e54
Jon <lundbyjon@hotmail.com>
2024-03-06 11:58:50 +0100

    Lavet indtil part 4 inkl. de to første opg af part 4

--- src/security/AuthProvider.tsx
+++ src/security/AuthProvider.tsx
@@ -1,5 +1,6 @@
 import { createContext, useState, ReactNode } from "react";
-import { fakeAuthProvider, User } from "../services/authFacade";
+// import { fakeAuthProvider, User } from "../services/authFacade";
+import { authProvider, User } from "../services/authFacade";
 import { useContext } from "react";
 import { LoginResponse, LoginRequest } from "../services/authFacade";
 
@@ -19,7 +20,8 @@ export default function AuthProvider({ children }: { children: ReactNode }) {
     const [username, setUsername] = useState<string | null>(initialUsername);
 
     const signIn = async (user_: LoginRequest) => {
-        return fakeAuthProvider.signIn(user_).then((user) => {
+        return authProvider.signIn(user_).then((user) => {//Call the signIn function in the authFacade with the user object
+            // console.log("user: ", user);
             setUsername(user.username);
             localStorage.setItem("username", user.username);
             localStorage.setItem("roles", JSON.stringify(user.roles));
