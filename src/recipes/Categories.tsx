2c9f03d6d894cf71955e4fe54b06a98c420b2072
Jon <lundbyjon@hotmail.com>
2024-03-07 00:38:48 +0100

    admin kan tilføje ny kategori

--- src/recipes/Categories.tsx
+++ src/recipes/Categories.tsx
@@ -1,18 +1,49 @@
 import { useState, useEffect } from "react";
-import { getCategories } from "../services/apiFacade";
+import { getCategories, addCategory } from "../services/apiFacade";
 import { Link } from "react-router-dom";
+import { useAuth } from "../security/AuthProvider";
 
 export const Categories = () => {
     const [categories, setCategories] = useState<Array<string>>();
+    const [categoryToAdd, setCategoryToAdd] = useState<string>("");
+    const auth = useAuth();
 
     useEffect(() => {
         getCategories().then((res) => setCategories(res));
     }, []);
 
+    // const handleAddCategory = () => {
+    //     if (categoryToAdd.length > 0) {
+    //         // setCategories([...categories, categoryToAdd]);
+    //         // setCategoryToAdd("");
+    //         // const newCategory = await addCategory(categoryToAdd);
+    //     }
+    // };
+
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        if (categoryToAdd.length > 0) {
+            await addCategory(categoryToAdd);
+            getCategories().then((res) => setCategories(res));
+        }
+    };
+
     return (
         <>
             <h2>Categories</h2>
             <p>Browse recipes by category.</p>
+            {auth.isLoggedInAs(["ADMIN"]) && (
+                <form onSubmit={handleSubmit}>
+                    add new category:
+                    <input
+                        value={categoryToAdd}
+                        type="text"
+                        style={{ margin: "5px" }}
+                        onChange={(e) => setCategoryToAdd(e.target.value)}
+                    ></input>
+                    <button type="submit">add</button>
+                </form>
+            )}
 
             <ul>
                 {categories?.map((category) => (
